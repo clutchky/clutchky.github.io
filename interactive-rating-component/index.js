365d9afe0d75eeefef7023e0bf3b9523e93e7b61
clutchky <clutchky@gmail.com>
2023-02-09 19:23:00 +0800

    updated solution

--- interactive-rating-component/index.js
+++ interactive-rating-component/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/styles.css';
-import App from './App';
+import './src/css/styles.css';
+import App from './src/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
