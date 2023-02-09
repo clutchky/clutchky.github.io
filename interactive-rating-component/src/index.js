f68761cf4209872196b46b365f45f070090883f5
clutchky <clutchky@gmail.com>
2023-02-09 19:30:03 +0800

    moved index

--- interactive-rating-component/src/index.js
+++ interactive-rating-component/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './src/css/styles.css';
-import App from './src/App';
+import './css/styles.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
