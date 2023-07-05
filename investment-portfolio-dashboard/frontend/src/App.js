66f689faebe2dedaec180184fd6209905af5afce
clutchky <clutchky@gmail.com>
2023-07-06 05:37:43 +0800

    reupload

--- investment-portfolio-dashboard/frontend/src/App.js
+++ investment-portfolio-dashboard/frontend/src/App.js
@@ -1,12 +1,13 @@
 import Stock from "./components/Stock";
 import { useEffect, useState } from "react";
 import stockServices from "./services/stocks";
+import ViewStock from "./components/ViewStock";
 
 function App() {
   const [stocklist, setStockList] = useState([]);
   const [newStock, setNewStock] = useState("");
   const [newPrice, setNewPrice] = useState("");
-  // const [viewedStock, setViewedStock] = useState(null);
+  const [viewedStock, setViewedStock] = useState(null);
 
   useEffect(() => {
     stockServices.getAll().then(res => {
@@ -54,7 +55,8 @@ function App() {
   }
 
   const viewStockOf = (id) => {
-    console.log("viewStockOf clicked");
+    console.log(`${viewedStock}`);
+    setViewedStock(true);
   }
 
   const editStockOf = (id) => {
@@ -90,50 +92,8 @@ function App() {
           />
         )}
       </div>
-      <div>
-          <hr></hr>
-          <p>Company name</p>
-          <label htmlFor="year">Fiscal Year </label>
-          <select name="year">
-            <option value="2023">2023</option>
-          </select>
-          <p>Price:</p>
-          <p>Sales Revenue:</p>
-          <p>Cost of Revenue:</p>
-          <p>Gross Profit:</p>
-          <p>Operating Expenses:</p>
-          <p>Operating Income:</p>
-          <p>Interest Expenses:</p>
-          <p>Income Taxes:</p>
-          <p>Net Income:</p>
-          <p>Earnings per share:</p>
-          <p>P/E Ratio:</p>
-          <p>Earnings Yield:</p>
-          <p>GPM Ratio:</p>
-          <p>OPM Ratio:</p>
-          <p>NPM Ratio:</p>
-          <p>EBIT:</p>
-          <p>Other Income:</p>
-          <p>Total Assets:</p>
-          <p>Total Liabilities:</p>
-          <p>Return on Equity:</p>
-          <p>Current Assets:</p>
-          <p>Current Liabilities:</p>
-          <p>Short-term debts:</p>
-          <p>Long-term debts:</p>
-          <p>Current Ratio:</p>
-          <p>Interest Coverage Ratio:</p>
-          <p>Working Capital Debt Ratio:</p>
-          <p>Operating Cashflow:</p>
-          <p>Investing Cashflow:</p>
-          <p>Financing Cashflow:</p>
-          <p>Capital Expenditures:</p>
-          <p>Free Cashflow:</p>
-          <p>Cashflow &gt; Long-term debt?</p>
-          <p>Dividend Yield:</p>
-          <p>Total Dividends:</p>
-          <p>Dividend Payout Ratio:</p>
-      </div>
+      
+      {viewedStock && <ViewStock />}
 
     </div>
   )
