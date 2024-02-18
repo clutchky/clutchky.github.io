0a3a438d2429a948a76571fc3edc5788651b45db
clutchky <clutchky@gmail.com>
2024-02-18 22:49:09 +0800

    added expiry for token, added token expiry error handler

--- ipdashboard/backend/utils/middleware.js
+++ ipdashboard/backend/utils/middleware.js
@@ -26,7 +26,11 @@ const errorHandler = (error, request, response, next) => {
             error: error.message
         });
     } else if (error.name === 'JsonWebTokenError') {
-        return response.status(400).json({ error: 'token missing or invalid' });
+        return response.status(401).json({ error: 'token missing or invalid' });
+    } else if (error.name === 'TokenExpiredError') {
+        return response.status(401).json({
+            error: 'token expired'
+        });
     }
 
     next(error);
