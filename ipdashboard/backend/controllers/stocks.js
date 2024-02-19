dea0b9fcceea43def426e8498dc80a90aebfaeda
clutchky <clutchky@gmail.com>
2024-02-19 16:25:14 +0800

    moved token feature to middleware

--- ipdashboard/backend/controllers/stocks.js
+++ ipdashboard/backend/controllers/stocks.js
@@ -3,14 +3,14 @@ const stocksRouter = require('express').Router();
 const Stock = require('../models/stock');
 const User = require('../models/user');
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization');
-    if (authorization && authorization.startsWith('Bearer ')) {
-        return authorization.replace('Bearer ', '');
-    }
+// const getTokenFrom = request => {
+//     const authorization = request.get('authorization');
+//     if (authorization && authorization.startsWith('Bearer ')) {
+//         return authorization.replace('Bearer ', '');
+//     }
 
-    return null;
-};
+//     return null;
+// };
 
 stocksRouter.get('/', async (request, response) => {
 
@@ -34,7 +34,7 @@ stocksRouter.get('/:id', async (request, response) => {
 // add a stockquote
 stocksRouter.post('/', async (request, response) => {
     const body = request.body;
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
 
     if (!decodedToken.id) {
         return response.status(401).json({ error: 'token invalid' });
